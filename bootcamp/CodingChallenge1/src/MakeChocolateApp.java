5ba758fd2c161555ab969e106fb0036c06cedaa4
Nick Law <nick@nickwlaw.com>
2018-12-06 18:47:54 -0500

    added comments and cleaned up code

--- bootcamp/CodingChallenge1/src/MakeChocolateApp.java
+++ bootcamp/CodingChallenge1/src/MakeChocolateApp.java
@@ -3,8 +3,11 @@ public class MakeChocolateApp {
 
 	public static void main(String[] args) {
 
+		// Greet the user and establish the purpose of the app
+		
 		System.out.println("Let's make some chocolate!\n");
-		String rules = "Here's the rules:\n" + "- One small chocolate bar weighs 1 kilo.\n"
+		String rules = "Here's the rules:\n" 
+				+ "- One small chocolate bar weighs 1 kilo.\n"
 				+ "- One BIG chocolate bar weighs 5 kilos.\n"
 				+ "- I'm going to give you orders for kilos of chocolate from 1 to 20,\n"
 				+ "  and you tell me how many BIG bars and small bars you have on hand.\n"
@@ -13,40 +16,49 @@ public class MakeChocolateApp {
 		System.out.println(rules);
 		boolean cont = true;
 		while (cont == true) {
+			// get a random order and display to user
 			int order = getOrder();
 			System.out.println("You have an order for " + order + " kilos of chocolate.\n");
+			// prompt user for small and large chocolate bars on hand
 			int smallBars = Console.getInt("How many small chocolate bars do you have? ");
 			int bigBars = Console.getInt("How many BIG chocolate bars do you have? ");
 			System.out.println();
+			// display number of small bars required to fill order
 			int result = makeChocolate(smallBars, bigBars, order);
 			if (result == -1)
-				System.out.println("You do not have enough chocolate to fill this order");
+				System.out.println("You do not have enough chocolate to fill this order.\n");
 			if (result >= 0)
 				System.out.println("You need " + result + " small bars of chocolate to fill this order.\n");
+			// ask user to continue
 			cont = Console.askToContinue();
 		}
 		System.out.println("Goodbye!");
 	}
 
+	// generate a random order for kilos of chocolate
 	private static int getOrder() {
 		int o = (int) (Math.random() * 100 / 5);
 		return o;
 	}
 
+	// perform logic that returns number of small bars needed to fill the order
+	// return -1 if user doesn't have enough small bars to fill the order
 	private static int makeChocolate(int sb, int bb, int goal) {
-		int a = 0;
 		int i = goal - (bb * 5);
-		if (i <= 0) return a;
+		if (i <= 0) {
+			i = 0;
+			return i;
+		}
 		if (i > 0) {
 			i -= sb;
 			if (i <= 0) {
-				a = sb + i;
-				return a;
+				i = sb + i;
+				return i;
 			} else {
-				a = -1;
-				return a;
+				i = -1;
+				return i;
 			}
 		}
-		return a;
+		return i;
 	}
 }
